bbee3b6a57f8a13f473f8c3687e69fb06f5fddc4
srijabal <srijaabal@gmail.com>
2024-04-11 21:57:15 +0530

    events,workshop, speaker cards,header

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,98 @@ export default function Header() {
     <>
       <div
         style={{
-          backgroundColor: "black",
+          backgroundColor: "#00000",
           position: "fixed",
           top: 0,
           width: "100vw",
           height: "5rem",
-          color: "white"
+          color: "white",
+          justifyContent: "space-between",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
         }}
       >
-    
+        <div style={{ marginLeft: "2rem" }}>
+          <img src="logo.svg" alt="E-Summit Logo" />
+        </div>
+        <div style={{ marginRight: "2rem" }}>
+          <a
+            href="#"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              marginRight: "2rem",
+            }}
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              marginRight: "2rem",
+            }}
+          >
+            About
+          </a>
+          <a
+            href="#"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              marginRight: "2rem",
+            }}
+          >
+            Events
+          </a>
+          <a
+            href="#"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              marginRight: "2rem",
+            }}
+          >
+            Sponsors
+          </a>
+          <a
+            href="#"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              marginRight: "2rem",
+            }}
+          >
+            Contact
+          </a>
+        </div>
+
+        <div
+          style={{ display: "flex", flexDirection: "row", marginRight: "2rem" }}
+        >
+          <button
+            style={{
+              borderColor: "#0ac182",
+              border: "4",
+              padding: "1rem",
+              borderRadius: "1rem",
+            }}
+          >
+            Login
+          </button>
+          <button
+            style={{
+              marginLeft: "1rem",
+              backgroundColor: "#0ac182",
+              padding: "1rem",
+              borderRadius: "1rem",
+            }}
+          >
+            Register
+          </button>
+        </div>
       </div>
     </>
   );
